194347cf2b43b28be67b16e122b9c9cd35aa10fd
Kozma Kristof <bechryko@gmail.com>
2025-03-29 10:59:07 +0100

    Add calculation logic to the language

--- CalculatorLang.g4
+++ CalculatorLang.g4
@@ -4,36 +4,44 @@ options {
     language = Java;
 }
 
-start: line*;
+start returns [double value]
+  : ( L=line { $value = $L.value; } )*;
 
-line
-  : expr EOL
-  | VAR ASSIGN line EOL
+line returns [double value]
+  : E=expr { $value = $E.value; } EOL
+  | V=var ASSIGN L=line { $value = CalculatorLang.setVarValue($V.key, $L.value); } EOL
   ;
 
-expr: expr_add;
-
-expr_add
-  : expr_mul ( OP_ADD expr_mul )*
+expr returns [double value]
+  : ADD=expr_add { $value = $ADD.value; }
   ;
 
-expr_mul
-  : expr_pwr ( OP_MUL expr_pwr )*
+expr_add returns [double value]
+  : MUL=expr_mul { $value = $MUL.value; } ( OP_ADD MUL=expr_mul { $value += $MUL.value; } )*
   ;
 
-expr_pwr
-  : term ( OP_PWR term )*
+expr_mul returns [double value]
+  : PWR=expr_pwr { $value = $PWR.value; } ( OP_MUL PWR=expr_pwr { $value *= $PWR.value; } )*
   ;
 
-term
-  : NUMBER
-  | VAR
-  | PAREN_OPENING expr PAREN_CLOSING
-  | prefixed_term
+expr_pwr returns [double value]
+  : T=term { CalculatorLang.initStack($T.value); } ( OP_PWR T=term { CalculatorLang.appendToStack($T.value); } )* { $value = CalculatorLang.getStackValue(); }
   ;
 
-prefixed_term
-  : ( OP_ADD )+ term
+term returns [double value]
+  : NUMBER { $value = Double.parseDouble("0" + $NUMBER.text); }
+  | V=var { $value = CalculatorLang.getVarValue($V.key); }
+  | '(' E=expr ')' { $value = $E.value; System.out.println($value); }
+  | PT=prefixed_term { $value = $PT.value; }
+  ;
+
+prefixed_term returns [double value]
+  : { CalculatorLang.initSignValue(); } ( OP_ADD { CalculatorLang.newSignValue($OP_ADD.text); } )+ T=term { $value = CalculatorLang.getSignedValue($T.value); }
+  ;
+
+var returns [String key]
+  : VAR { $key = $VAR.text; }
+  | VAR_INTERPOLATE_PREFIX L=line VAR_INTERPOLATE_SUFFIX { $key = CalculatorLang.interpolateVarKey($VAR_INTERPOLATE_PREFIX.text, $VAR_INTERPOLATE_SUFFIX.text, $L.value); }
   ;
 
 WS: [ \t\r\n]+ -> skip;
@@ -42,8 +50,10 @@ NUMBER: ([0-9]*[.])?[1-9][0-9]*;
 OP_ADD: ('+'|'-');
 OP_MUL: ('*'|'/');
 OP_PWR: '^';
-VAR: 'M';
+VAR_PREFIX: 'M';
+VAR: VAR_PREFIX[0-9]*;
+VAR_INTERPOLATE_PREFIX: VAR_PREFIX[0-9]*'{';
+VAR_INTERPOLATE_SUFFIX: '}'[0-9]*;
 ASSIGN: '=';
 PAREN_OPENING: '(';
 PAREN_CLOSING: ')';
-
--- CalculatorLang.java
+++ CalculatorLang.java
@@ -1,14 +1,88 @@
 import java.io.IOException;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 import org.antlr.v4.runtime.*;
 
 public class CalculatorLang {
-   public static void main(String[] args) throws IOException {
-      var lexer = new PolynomialCalculatorLangLexer(new ANTLRFileStream(args[0]));
-      var tokens = new CommonTokenStream(lexer);
-      var parser = new PolynomialCalculatorLangParser(tokens);
-      var startCtx = parser.start();
+    private static final Map<String, Double> M = new HashMap<>();
+    private static final Stack<Double> pwrStack = new Stack<>();
+    private static String sign = "+";
 
-      System.out.println(startCtx.toStringTree(parser));
-   }
+    public static void main(String[] args) throws IOException {
+        var lexer = new CalculatorLangLexer(new ANTLRFileStream(args[0]));
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new CalculatorLangParser(tokens);
+        var startCtx = parser.start();
+
+        System.out.println(startCtx.toStringTree(parser));
+        System.out.println(startCtx.value);
+        System.out.println(savedVariableMapToString());
+    }
+
+    public static String interpolateVarKey(String prefix, String suffix, double value) {
+        var sb = new StringBuilder(prefix);
+        sb.deleteCharAt(sb.toString().length() - 1);
+        String parsedValue = value % 1 == 0 ? "" + ((int)value) : "" + value;
+        sb.append(parsedValue);
+        sb.append(suffix.substring(1));
+        return sb.toString();
+    }
+
+    public static double setVarValue(String varName, double value) {
+        M.put(varName, value);
+        return value;
+    }
+
+    public static double getVarValue(String varName) {
+        Double value = M.get(varName);
+        if(value == null) {
+            return 0;
+        }
+        return value;
+    }
+
+    public static void initStack(double firstValue) {
+        pwrStack.clear();
+        appendToStack(firstValue);
+    }
+
+    public static void appendToStack(double value) {
+        pwrStack.add(value);
+    }
+
+    public static double getStackValue() {
+        double value = pwrStack.pop();
+        while(!pwrStack.empty()) {
+            value = Math.pow(pwrStack.pop(), value);
+        }
+        return value;
+    }
+
+    public static void initSignValue() {
+        sign = "+";
+    }
+
+    public static void newSignValue(String newSign) {
+        if("-".equals(newSign)) {
+            sign = "+".equals(sign) ? "-" : "+";
+        }
+    }
+
+    public static double getSignedValue(double value) {
+        return "+".equals(sign) ? value : -value;
+    }
+
+    private static String savedVariableMapToString() {
+        var sb = new StringBuilder("Saved variables: { ");
+        for(var e : M.entrySet()) {
+            sb.append(e.getKey());
+            sb.append(": ");
+            sb.append(e.getValue());
+            sb.append(", ");
+        }
+        sb.deleteCharAt(sb.toString().length() - 2);
+        sb.append("}");
+        return sb.toString();
+    }
 }
